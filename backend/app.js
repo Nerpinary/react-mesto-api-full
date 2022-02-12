18e49bcee377f716269bed05c696d4331706dff1
Максим Ситников <maksimsitnikov@Air-Maksim.Dlink>
2022-02-12 15:56:02 +0300

    add logger

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const { login } = require('./controllers/users');
 const { createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not_found_error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -21,6 +22,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -44,6 +46,8 @@ app.use(auth);
 app.use('/', userRouter);
 app.use('/', cardsRouter);
 app.use('*', () => { throw new NotFoundError('Запрашиваемый ресурс не найден.'); });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((err, request, response, next) => {
